742d99c52d7ede8cc1db4ec77c956c99417455d7
doomhex <guitarpro1657@.gami.com>
2025-12-18 10:41:24 +0500

    salom

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,22 @@
 import React from "react";
 
 const Header = () => {
-  return <div>hello</div>;
+  return (
+    <div>
+      <div className="container flex justify-between">
+        <div className=" border font-[700] text-[35px] text-[#ffffff] ml-[30px] block bg-red-500">
+          <h1 className="">trxvl.</h1>
+        </div>
+        <div className=" ">
+          <a href="">Home</a>
+          <a href="">Stays</a>
+          <a href="">Flights</a>
+          <a href="">Packages</a>
+          <a href="">Sign Up</a>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Header;
